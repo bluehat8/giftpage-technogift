3f8ab7e8603bb827cd8dc03158fd24904f8045c0
Ziromap <ziromapdesigns@gmail.com>
2022-09-30 15:51:23 -0600

    Se arreglaron detalles

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,6 @@
 // Get the modal
 var modal1 = document.getElementById("myModal");
-var modalContent=document.getElementById("modal-content");
+var modalContent = document.getElementById("modal-content");
 
 // Get the button that opens the modal
 var btn1 = document.getElementById("myBtn");
@@ -9,40 +9,40 @@ var btn1 = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn1.onclick = function() {
+btn1.onclick = function () {
   modal1.style.display = "block";
   modal1.style.visibility = "visible";
   modal1.style.width = "100%";
-  modalContent.style.margin="0% 40%";
-  modalContent.style.width="60%";
-
+  modalContent.style.margin = "0% 40%";
+  modalContent.style.width = "60%";
+  modal1.style.overflowY = "auto";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal1.style.visibility = "hidden";
-  modalContent.style.margin="0% 100%";
-  modalContent.style.width="100%";
-
+  modalContent.style.margin = "0% 100%";
+  modalContent.style.width = "100%";
+  modal1.style.overflowY = "hidden";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal1) {
     modal1.style.visibility = "hidden";
-    modalContent.style.margin="0% 100%";
-    modalContent.style.width="100%";
-
+    modalContent.style.margin = "0% 100%";
+    modalContent.style.width = "100%";
+    modal1.style.overflowY = "hidden";
   }
 }
 
 // Get the <span> element that closes the modal
 var span_close = document.getElementsById("close");
-span_close.onclick = function() {
+span_close.onclick = function () {
   modal1.style.display = "none";
   modal1.style.visibility = "hidden";
-  modalContent.style.margin="0% 100%";
-  modalContent.style.width="100%";
+  modalContent.style.margin = "0% 100%";
+  modalContent.style.width = "100%";
 }
 
 
@@ -56,17 +56,17 @@ var btn3 = document.getElementById("myBtn3");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn3.onclick = function() {
+btn3.onclick = function () {
   modal2.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal2.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal2) {
     modal2.style.display = "none";
   }
@@ -74,6 +74,6 @@ window.onclick = function(event) {
 
 // Get the <span> element that closes the modal
 var span_close = document.getElementsById("close");
-span_close.onclick = function() {
+span_close.onclick = function () {
   modal1.style.display = "none";
 }
