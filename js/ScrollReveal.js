b014d5c40ac3b01fc957d4e039c289d67fd35443
kennethT2014 <kenneth.teller2014@gmail.com>
2022-10-20 07:51:38 -0600

    Landing audifono y correo

---
+++ js/ScrollReveal.js
@@ -0,0 +1,6 @@
+window.sr=ScrollReveal({
+    distance: '25px',
+    duration: 2500,
+    reset: true
+});
+sr.reveal(".containerHeader,.Item",{delay:200, origin:'bottom'});
---
+++ js/audifonos.js
@@ -0,0 +1,13 @@
+gsap.from('.Audifonos01', {
+    duration: 5,
+    x: 600,
+    delay: 1,
+    ease: "elastic.out(1, 0.3)",
+    y: -50
+});
+gsap.from('.Audifono02Blanco', {
+    duration: 5,
+    x: -600,
+    delay: 1,
+    ease: "elastic.out(1, 0.3)"
+});
